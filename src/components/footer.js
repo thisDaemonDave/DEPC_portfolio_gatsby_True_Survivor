7451448aa83cf41251f94724c14b046e7e7c947a
David Pineda <david.pineda@deparadise.com>
2020-01-03 00:38:41 -0800

    Setup/webflow layout integration (#3)
    * Include standing image and font assets from webflow export bundle
    
    * include gatsby-browser and Webflow generated styles
    
    * Add className updates to show head and footer fade effects plus body style consolidation (DO NOT USE .body to assign styles)

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,10 @@ import React from 'react';
 const Footer = () => (
 	<>
 		<footer className="site-footer">
-			<h3>Here is some footer Content</h3>
+			<div className="footer-content">
+				<h3>Here is some footer Content</h3>
+			</div>
+			<div className="footer-fade"></div>
 		</footer>
 	</>
 );
--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
 const Header = () => (
-	<header>
+	<header className="site-header">
+		<div className="header-fade"></div>
 		{/* site-brand-name */}
 		<span className="by-line">The Portfolio of David Pineda on</span>
 		<h1>DEParadise</h1>
