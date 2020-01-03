854a12145aa93904ad029374d2bb1f08e98ec968
David Pineda <david.pineda@deparadise.com>
2020-01-02 21:55:13 -0800

    START base Footer component

---
+++ src/components/footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => (
+	<>
+		<footer className="site-footer">
+			<h3>Here is some footer Content</h3>
+		</footer>
+	</>
+);
+
+export default Footer;
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,13 @@
 // STANDARD BASE LAYOUT
 import React from 'react';
 import Header from './header';
+import Footer from './footer';
 
 const Layout = ({ children }) => (
 	<>
 		<Header></Header>
 		<main>{children}</main>
+		<Footer></Footer>
 	</>
 );
 
