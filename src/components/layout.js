da2c701e81be8cd6752e2d1e4bf05a43b764e4df
David Pineda <david.pineda@deparadise.com>
2020-01-02 20:43:47 -0800

    START  standard_base_layout and implement on the sandbox/new_page

---
+++ src/components/layout.js
@@ -0,0 +1,10 @@
+// STANDARD BASE LAYOUT
+import React from 'react';
+
+const Layout = () => (
+	<>
+		<h1>NUB</h1>
+	</>
+);
+
+export default Layout;
--- src/pages/sandbox/new_page.js
+++ src/pages/sandbox/new_page.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import Layout from '../../components/layout';
 
 export default () => (
-	<>
+	<Layout>
 		<h1>Heading for the sandbox NEW_PAGE</h1>
 		<p>This is some test copy that needs to be some lorem ipsum</p>
-	</>
+	</Layout>
 );
