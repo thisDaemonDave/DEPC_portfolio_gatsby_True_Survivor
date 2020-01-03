b6e4afbb7d019c72d99f39d1b6b48bd0031aecf7
David Pineda <david.pineda@deparadise.com>
2020-01-02 22:05:35 -0800

    START HeroMural component

---
+++ src/components/hero/mural.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const HeroMural = () => (
+	<>
+		<section className="hero-mural">Hero Mural goes here</section>
+	</>
+);
+
+export default HeroMural;
--- src/pages/sandbox/new_page.js
+++ src/pages/sandbox/new_page.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Layout from '../../components/layout';
+import HeroMural from '../../components/hero/mural';
 
 export default () => (
 	<Layout>
+		<HeroMural />
 		<h1>Heading for the sandbox NEW_PAGE</h1>
 		<p>This is some test copy that needs to be some lorem ipsum</p>
 	</Layout>
