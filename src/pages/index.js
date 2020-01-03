52dbab7e9ec25f7612ddd108b7f08e0638b71f94
David Pineda <david.pineda@deparadise.com>
2020-01-02 19:16:46 -0800

    no message

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default () => (
+	<>
+		<h1>Hello World!</h1>
+		<p>
+			This is my portfolio! Named after the song from "Kung Fury" - "True
+			Survivor"
+		</p>
+	</>
+);
