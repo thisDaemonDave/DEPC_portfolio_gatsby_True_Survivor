657d29d0a0efc9041aebee1a610a8d69b5ea37ea
David Pineda <david.pineda@deparadise.com>
2020-01-02 20:28:00 -0800

    START Header component

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => (
+	<header>
+		{/* site-brand-name */}
+		<span class="by-line">The Portfolio of David Pineda on</span>
+		<h1>DEParadise</h1>
+		<h2>Design / Web * Creative</h2>
+		{/* navigation */}
+	</header>
+);
+
+export default Header;
